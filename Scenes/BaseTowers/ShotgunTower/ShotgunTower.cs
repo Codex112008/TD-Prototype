0112d9ce8cdf32f63c87ab2f2d9246ed2bd1aaf7
Codex112008 <codexofcode1120@gmail.com>
2025-08-23 22:50:21 +0800

    Started doing shotgun tower

---
+++ Scenes/BaseTowers/ShotgunTower/ShotgunTower.cs
@@ -0,0 +1,84 @@
+using Godot;
+using System;
+
+public partial class ShotgunTower : Tower
+{
+	[Export] private Marker2D _firePoint;
+    [Export] private Marker2D _pivotPoint;
+    [Export] private float _rotateSpeed;
+
+    private Timer _fireTimer;
+    private Enemy _target = null;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _fireTimer = new Timer
+        {
+            OneShot = true,
+        };
+        AddChild(_fireTimer);
+
+        _fireTimer.WaitTime = 1f / GetFinalTowerStats()[TowerStat.FireRate];
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        if (Projectile != null && Projectile.Effects.Count > 0 && !IsBuildingPreview)
+        {
+            if (_fireTimer.WaitTime != 1f / GetFinalTowerStats()[TowerStat.FireRate])
+            {
+                _fireTimer.WaitTime = 1f / GetFinalTowerStats()[TowerStat.FireRate];
+            }
+
+            if (GetTree().GetNodeCountInGroup("Enemy") > 0)
+            {
+                Enemy firstEnemy = FindFirstEnemy();
+                if (_target != firstEnemy)
+                {
+                    _target = firstEnemy;
+                }
+
+                if (_target != null)
+                {
+                    Vector2 dir = GetCenteredGlobalPosition().DirectionTo(_target.GlobalPosition + _target.Velocity * (GetCenteredGlobalPosition().DistanceTo(_target.GlobalPosition) / 250f));
+                    float targetAngle = dir.Angle() + Mathf.Pi / 2f;
+                    _pivotPoint.Rotation = Mathf.LerpAngle(_pivotPoint.Rotation, targetAngle, _rotateSpeed * (float)delta);
+
+                    if (_fireTimer.IsStopped() && Mathf.Abs(Mathf.Wrap(targetAngle - _pivotPoint.Rotation, -Mathf.Pi, Mathf.Pi)) <= Mathf.Pi / 16f)
+                    {
+                        _pivotPoint.Rotation = targetAngle;
+                        Fire();
+                        _fireTimer.Start();
+                    }
+                }
+            }
+        }
+    }
+
+    private Enemy FindFirstEnemy()
+    {
+        Enemy firstEnemy = null;
+        foreach (Node node in GetTree().GetNodesInGroup("Enemy"))
+        {
+            if (node is Enemy enemy)
+            {
+                float distanceToEnemy = GetCenteredGlobalPosition().DistanceTo(enemy.GlobalPosition);
+                if ((firstEnemy == null || enemy.PathArray.Count < firstEnemy.PathArray.Count) && distanceToEnemy <= GetRangeInTiles())
+                {
+                    firstEnemy = enemy;
+                }
+            }
+        }
+
+        return firstEnemy;
+    }
+
+    protected override void Fire()
+    {
+        Projectile.InstantiateProjectile(GetFinalTowerStats(), _firePoint);
+    }
+}
