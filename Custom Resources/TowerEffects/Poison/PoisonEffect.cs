c79355bf3b7b6e36d2c80a4ab3d255457f354684
Codex112008 <codexofcode1120@gmail.com>
2026-01-16 22:39:23 +0800

    Poison one liner update
    Made poison effect do 0.2x poison damage as well
    
    Added an icon for bleed

--- Custom Resources/TowerEffects/Poison/PoisonEffect.cs
+++ Custom Resources/TowerEffects/Poison/PoisonEffect.cs
@@ -7,5 +7,6 @@ public partial class PoisonEffect : TowerEffect
     protected override void ApplyEffectCore(Dictionary<TowerStat, float> stats, Enemy target)
     {
         target.AddStatusEffectStacks(StatusEffect.Poison, _finalStats[TowerStat.Damage]);
+        target.TakeDamage(_finalStats[TowerStat.Damage] * 0.2f, DamageType.Poison, true);
     }
 }
